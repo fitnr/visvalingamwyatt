bb1af4014e5f445a5541936dd7f30c9b83165d43
fitnr <fitnr@fakeisthenewreal>
2017-12-03 16:20:23 -0500

    simplify tests, add test for __main__simplify
    add python 3.6 to tests

--- tests/test_vw.py
+++ tests/test_vw.py
@@ -7,13 +7,16 @@
 # Licensed under the MIT license:
 # http://www.opensource.org/licenses/MIT-license
 # Copyright (c) 2015, fitnr <contact@fakeisthenewreal.org>
+import os
 import json
-import numpy as np
 import unittest
+from collections import namedtuple
+import numpy as np
 import visvalingamwyatt as vw
-
+from visvalingamwyatt import __main__ as cli
 
 class TestCase(unittest.TestCase):
+
     def setUp(self):
         with open('tests/data/sample.json') as f:
             self.fixture = json.load(f).get('features')[0]
@@ -47,25 +50,17 @@ class TestCase(unittest.TestCase):
             s = vw.simplify_feature(self.fixture, ratio=float(j) / 10)
             assert len(r['geometry']['coordinates']) <= len(s['geometry']['coordinates'])
 
-
     def testSimplifyFeatureNumber(self):
         self.standard(number=10)
 
     def test3dCoords(self):
         coordinates = [[0.0, 0.0, 0.0], [1.1, 0, 1], [2.1, 3, 0], [4.1, 5, 10], [1.1, 2, 0], [5.1, 2, 0]]
         a = vw.simplify(coordinates)
-        assert a[0] == [0, 0, 0]
-        assert len(a) <= len(coordinates)
+        self.assertEqual(a[0], [0, 0, 0])
+        self.assertLessEqual(len(a), len(coordinates))
 
     def testSimplifyTupleLike(self):
-        class Point(object):
-            def __init__(self, x, y):
-                self.x = x
-                self.y = y
-
-            def __iter__(self):
-                yield self.x
-                yield self.y
+        Point = namedtuple("Point", ("x", "y"))
 
         # coordinates are in the shape
         #
@@ -76,11 +71,11 @@ class TestCase(unittest.TestCase):
         # so b and d are eliminated
 
         a, b, c, d, e = Point(0, 0), Point(1, 1), Point(2, 2), Point(3, 1), Point(4, 0)
-        input = [a, b, c, d, e]
+        inp = [a, b, c, d, e]
         expected_output = np.array([a, c, e])
 
-        actual_output = vw.simplify(input, threshold=0.001)
-        assert np.array_equal(actual_output, expected_output)
+        actual_output = vw.simplify(inp, threshold=0.001)
+        self.assertTrue(np.array_equal(actual_output, expected_output))
 
     def testSimplifyIntegerCoords(self):
         # coordinates are in the shape
@@ -92,8 +87,26 @@ class TestCase(unittest.TestCase):
         # so b and d are eliminated
 
         a, b, c, d, e = (0, 0), (1, 1), (2, 2), (3, 1), (4, 0)
-        input = [a, b, c, d, e]
+        inp = [a, b, c, d, e]
         expected_output = np.array([a, c, e])
 
-        actual_output = vw.simplify(input, threshold=0.001)
-        assert np.array_equal(actual_output, expected_output)
+        actual_output = vw.simplify(inp, threshold=0.001)
+        self.assertTrue(np.array_equal(actual_output, expected_output))
+
+    def testCli(self):
+        pass
+
+    def testSimplify(self):
+        try:
+            output = 'tmp.json'
+            cli.simplify('tests/data/sample.json', output, number=9)
+
+            self.assertTrue(os.path.exists(output))
+
+            with open('tmp.json', 'r') as f:
+                result = json.load(f)
+                coords = result['features'][0]['geometry']['coordinates']
+                self.assertEqual(len(coords), 10)
+
+        finally:
+            os.remove(output)
